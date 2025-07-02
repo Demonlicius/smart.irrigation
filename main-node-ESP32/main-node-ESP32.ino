a14f914fcb827f8b4953bba5eb6ac94efa162d06
Jesús Alexis Rendón Alvarado <rendonalvaradojesusalexis@gmail.com>
2025-07-01 23:06:59 -0600

    Implementation of LoRa with ESPNOW

--- main-node-ESP32/main-node-ESP32.ino
+++ main-node-ESP32/main-node-ESP32.ino
@@ -1,12 +1,29 @@
+#include <SPI.h>
+#include <LoRa.h>
 #include <esp_now.h>
 #include <WiFi.h>
+
+
+// Configuración de pines ESP32-WROOM-32
+#define NSS 5
+#define RESET 2
+#define SCK 18
+#define MISO 19
+#define MOSI 23
+
+
+
+// Direcciones LoRa
+#define LOCAL_ADDRESS 0xAA    // Dirección del transmisor (ESP32)
+#define RECEIVER_ADDRESS 0xFF // Dirección del receptor
+
+int msgCount = 0;
 
 typedef struct struct_message {
   float temperatura;
   float temperaturaA;
   float humedadA;
   int humedad;
-
   uint8_t idNodo;
 } struct_message;
 
@@ -19,24 +36,23 @@ void OnDataRecv(const esp_now_recv_info_t *recv_info, const uint8_t *incomingDat
   snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X",
            recv_info->src_addr[0], recv_info->src_addr[1], recv_info->src_addr[2],
            recv_info->src_addr[3], recv_info->src_addr[4], recv_info->src_addr[5]);
-           
-  Serial.print("Nodo ID: ");
-  Serial.print(datosRecibidos.idNodo);
-  Serial.println("");
-  Serial.print("Temperatura: ");
-  Serial.print(datosRecibidos.temperatura);
-  Serial.print("°C | Humedad: ");
-  Serial.print(datosRecibidos.humedad);
-  Serial.println("%");
-    Serial.print("Temperatura ambiental: ");
-  Serial.print(datosRecibidos.temperaturaA);
-  Serial.print("°C | Humedad ambiental: ");
-  Serial.print(datosRecibidos.humedadA);
-  Serial.println("%");
 }
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  // Configurar SPI con pines específicos
+  SPI.begin(SCK, MISO, MOSI, NSS);
+  // Configurar LoRa
+  LoRa.setPins(NSS, RESET);
+
+  if (!LoRa.begin(915E6)) {
+    Serial.println("Error al iniciar LoRa");
+    while (1);
+  }
+  
+  LoRa.setTxPower(20);  // Máxima potencia (20 dBm)
+  Serial.println("Transmisor iniciado");
+
   WiFi.mode(WIFI_STA);
   Serial.print("MAC del nodo principal: ");
   Serial.println(WiFi.macAddress());
@@ -47,8 +63,29 @@ void setup() {
   }
 
   esp_now_register_recv_cb(OnDataRecv);
-}
 
+}
+void sendMessage(String outgoing) {
+  LoRa.beginPacket();
+  LoRa.write(RECEIVER_ADDRESS);  // Dirección destino
+  LoRa.write(LOCAL_ADDRESS);     // Dirección origen
+  LoRa.write(msgCount);          // Contador de mensajes
+  LoRa.write(outgoing.length()); // Longitud del mensaje
+  LoRa.print(outgoing);          // Contenido del mensaje
+  LoRa.endPacket();
+  
+  msgCount++;
+}
 void loop() {
   
+  String mensaje = String(LOCAL_ADDRESS) + "," + datosRecibidos.idNodo + "," + datosRecibidos.temperatura + "," 
+  + datosRecibidos.humedad + "," + datosRecibidos.temperaturaA + "," + datosRecibidos.humedadA;
+  
+  // Enviar mensaje
+  sendMessage(mensaje);
+  
+  Serial.print("Enviando: ");
+  Serial.println(mensaje);
+  
+  delay(1000); // Esperar 1 segundos entre transmisiones
 }
