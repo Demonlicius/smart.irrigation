03234d4f603992c7dd6fed7539f6e9914d5ad6ef
Jesús Alexis Rendón Alvarado <rendonalvaradojesusalexis@gmail.com>
2025-07-02 03:17:27 -0600

    Update sensor-Node-ESP32.ino
    Add ACK At layer aplication for control of msg

--- sensor-Node-ESP32/sensor-Node-ESP32.ino
+++ sensor-Node-ESP32/sensor-Node-ESP32.ino
@@ -17,7 +17,7 @@ DHTesp dht;
 // Dirección MAC del receptor
 uint8_t broadcastAddress[] = {0x78, 0x42, 0x1c, 0x67, 0xAA, 0x2C};
 
-// Estructura
+// Estructura mensaje de datos
 typedef struct struct_message {
   float temperatura;
   float temperaturaA;
@@ -28,6 +28,25 @@ typedef struct struct_message {
 
 struct_message datosSensor;
 
+// Estructura ACK
+typedef struct struct_ack {
+  uint8_t idNodo;
+  bool recibido;
+} struct_ack;
+
+struct_ack ackMsg;
+volatile bool ackRecibido = false;
+
+// Callback recepción de ACK
+void OnDataRecv(const esp_now_recv_info_t *recv_info, const uint8_t *incomingData, int len) {
+  memcpy(&ackMsg, incomingData, sizeof(ackMsg));
+  if (ackMsg.recibido && ackMsg.idNodo == datosSensor.idNodo) {
+    ackRecibido = true;
+  }
+}
+
+
+// Callback de envío
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.print("Envío: ");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "OK" : "FAIL");
@@ -40,11 +59,15 @@ void initEspNow() {
     Serial.println("Error init ESP-NOW");
     return;
   }
+
   esp_now_register_send_cb(OnDataSent);
+  esp_now_register_recv_cb(OnDataRecv);
+
   esp_now_peer_info_t peerInfo = {};
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
+
   esp_now_add_peer(&peerInfo);
 }
 
@@ -52,19 +75,16 @@ void setup() {
   Serial.begin(9600);
   sensors.begin();
   dht.setup(pinDHT, DHTesp::DHT11);
-  datosSensor.idNodo = 2; //cambiar id en cada dispositivo nuevo
+  datosSensor.idNodo = 2; // Cambiar en cada nodo
   initEspNow();
 }
 
 void loop() {
-
-
   int lectura = analogRead(SENSOR_HUMEDAD);  
   int humedads = map(lectura, 1200, 4095, 100, 0); 
 
   sensors.requestTemperatures();
   float tempC = sensors.getTempCByIndex(0);
-
   TempAndHumidity data = dht.getTempAndHumidity();
 
   datosSensor.temperatura = tempC;
@@ -72,9 +92,15 @@ void loop() {
   datosSensor.humedadA = data.humidity;
   datosSensor.humedad = humedads;
 
-  delay(100); 
-
+  ackRecibido = false;
   esp_now_send(broadcastAddress, (uint8_t*)&datosSensor, sizeof(datosSensor));
 
-  delay(2000); 
+  // Esperar ACK hasta 500 ms
+  unsigned long t0 = millis();
+  while (!ackRecibido && millis() - t0 < 500) {
+    delay(10);
+  }
+
+
+  delay(1000);
 }
